fb4405de3f0152f821ff2022b8850f1700b23369
Marcos <marvisilva202@gmail.com>
2023-04-06 10:06:07 -0300

    finalizando query de pagamentos, SQLite não possui função percentile consegui resolver, mas recomendo usar um db mais potente como postgresql ou mysql

--- src/etl/pagamentos.sql
+++ src/etl/pagamentos.sql
@@ -1,12 +1,17 @@
-WITH tb_join AS (
-    SELECT t2.*,
-        t3.idVendedor
+WITH tb_pedidos AS (
+    SELECT DISTINCT t1.idPedido,
+        t2.idVendedor
     FROM pedido AS t1
+        LEFT JOIN item_pedido AS t2 ON t1.idPedido = t2.idPedido
+    WHERE dtPedido < '2018-01-01'
+        AND dtPedido >= DATE('2018-01-01', '-6 months')
+        AND idVendedor IS NOT NULL
+),
+tb_join AS (
+    SELECT t1.idVendedor,
+        t2.*
+    FROM tb_pedidos AS t1
         LEFT JOIN pagamento_pedido AS t2 ON t1.idPedido = t2.idPedido
-        LEFT JOIN item_pedido AS t3 ON t1.idPedido = t3.idPedido
-    WHERE t1.dtPedido <= '2018-01-01'
-        AND t1.dtPedido >= DATE('2018-01-01', '-6 months')
-        AND t3.idVendedor IS NOT NULL
 ),
 tb_group AS (
     SELECT idVendedor,
@@ -18,103 +23,130 @@ tb_group AS (
         descTipoPagamento
     ORDER BY idVendedor,
         descTipoPagamento
+),
+tb_summary AS (
+    SELECT idVendedor,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS qtde_boleto_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS qtde_credit_card_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS qtde_voucher_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS qtde_debit_card_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS valor_boleto_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS valor_credit_card_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS valor_voucher_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) AS valor_debit_card_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_boleto_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_credit_card_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_voucher_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_debit_card_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(vlPedidoMeioPagamento) AS pct_valor_boleto_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(vlPedidoMeioPagamento) AS pct_valor_credit_card_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(vlPedidoMeioPagamento) AS pct_valor_voucher_pedido,
+        sum(
+            CASE
+                WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento
+                ELSE 0
+            END
+        ) / sum(vlPedidoMeioPagamento) AS pct_valor_debit_card_pedido
+    FROM tb_group
+    GROUP BY idVendedor
+),
+tb_cartao AS (
+    SELECT idVendedor,
+        AVG(nrParcelas) AS avgQtdeParcelas,
+        ROUND(
+            PERCENT_RANK() OVER(
+                PARTITION BY nrParcelas
+                ORDER BY idVendedor
+            ) * 100,
+            0
+        ) AS medianQtdeParcelas,
+        MAX(nrParcelas) AS maxQtdeParcelas,
+        MIN(nrParcelas) AS minQtdeParcelas
+    FROM tb_join
+    WHERE descTipoPagamento = 'credit_card'
+    GROUP BY idVendedor
 )
-SELECT idVendedor,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS qtde_boleto,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS qtde_credit_card,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS qtde_voucher,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS qtde_debit_card,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS valor_boleto,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS valor_credit_card,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS valor_voucher,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) AS valor_debit_card,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_boleto,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_credit_card,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_voucher,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(qtdePedidoMeioPagamento) AS pct_qtd_debit_card,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(vlPedidoMeioPagamento) AS pct_valor_boleto,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(vlPedidoMeioPagamento) AS pct_valor_credit_card,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(vlPedidoMeioPagamento) AS pct_valor_voucher,
-    sum(
-        CASE
-            WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento
-            ELSE 0
-        END
-    ) / sum(vlPedidoMeioPagamento) AS pct_valor_debit_card
-FROM tb_group
-GROUP BY 1
+SELECT '2018-01-01' as dtReference,
+    t1.*,
+    t2.avgQtdeParcelas,
+    t2.idVendedor,
+    t2.medianQtdeParcelas,
+    t2.maxQtdeParcelas,
+    t2.minQtdeParcelas
+FROM tb_summary AS t1
+    LEFT JOIN tb_cartao AS t2 On t1.idVendedor = t2.idVendedor
